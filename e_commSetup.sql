b650bad6d4eff6dab427eec89f0b7f2d31c0c8f5
Chong Kwang Chen <kwangchen.coding@gmail.com>
2025-03-07 21:37:42 +0800

    update

--- e_commSetup.sql
+++ e_commSetup.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS UserAccount (
     email VARCHAR(100) NOT NULL UNIQUE,      
     password_hash VARCHAR(255) NOT NULL,     
     first_name VARCHAR(50),                  
-    last_name VARCHAR(50),                   
+    last_name VARCHAR(50),
+    phone_number VARCHAR(15) NULL, 
     role ENUM('producer','learner','admin') DEFAULT 'learner',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
